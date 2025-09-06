c9e464140056b5a08452ad1211ff86644f753506
Nazmul Islam Sakib <sakibsarkar>
2025-09-06 12:24:39 +0600

    feat: Updae

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 const error = () => {
-  return <div>ok no more error</div>;
+  return <div className="flex h-dvh w-dvh items-center justify-center">Something went wrong</div>;
 };
 
 export default error;
